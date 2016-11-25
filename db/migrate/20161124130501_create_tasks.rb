b69da953c16202c4181ea91669f317ba6f30f4cd
Berkan Chelikhan <bc00208@surrey.ac.uk>
2016-11-25 18:19:35 +0000

    Added AJAX

--- db/migrate/20161124130501_create_tasks.rb
+++ db/migrate/20161124130501_create_tasks.rb
@@ -3,7 +3,6 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.belongs_to :note, index: true, foreign_key: true, null:false
       t.string :title, null:false
-
       t.timestamps null: false
     end
   end
